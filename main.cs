dc5ad148d11064b98b94504a255d291a06bd1501
Tate Christians <97200267+tsizzle987@users.noreply.github.com>
2022-01-11 22:19:31 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,1 +1,11 @@
-
+using System;
+ 
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
